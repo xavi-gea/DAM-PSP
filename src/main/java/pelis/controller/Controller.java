2f6c95feba48c0b9cdab55e56441a98755c267af
Xavi <144837551+xavi-gea@users.noreply.github.com>
2025-02-10 18:25:39 +0100

    Done

--- src/main/java/pelis/controller/Controller.java
+++ src/main/java/pelis/controller/Controller.java
@@ -1,6 +1,10 @@
 package pelis.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +16,60 @@ public class Controller {
 	Pelis pelis = new Pelis();
 
 	@GetMapping("/APIpelis/t")
-	String getFilm(@RequestParam(value = "id") String filmID) {
+	public ResponseEntity<String> getFilm(@RequestParam(value = "id") String filmID) {
 		
-		return pelis.getAllPelis();
+		String result = "";
 		
-//		if (filmID.equals("all")) {
-//			
-//			return pelis.getAllPelis();
-//			
-//		}else {
-//			
-//			return pelis.getPeli(filmID);
-//		}
+		if (filmID.equals("all")) {
+			
+			result = pelis.getAllPelis();
+			
+		}else {
+			
+			result = pelis.getPeli(filmID);
+		}
+		
+		if (result != null) {
+			
+			return ResponseEntity.status(HttpStatus.OK).body(result);
+			
+		}else {
+			
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+		}
+	}
+	
+	@PostMapping("/APIpelis/nuevaResenya")
+	public ResponseEntity<Object> newReview(@RequestBody String filmReviewBody){
+		
+		boolean reviewWritten = false;
+		
+		reviewWritten = pelis.postFilmReview(filmReviewBody);
+		
+		if (reviewWritten) {
+			
+			return ResponseEntity.status(HttpStatus.OK).build();
+			
+		}else {
+			
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+	
+	@PostMapping("/APIpelis/nuevaPeli")
+	public ResponseEntity<Object> newPeli(@RequestBody String filmBody){
+		
+		boolean filmWritten = false;
+		
+		filmWritten = pelis.postFilm(filmBody);
+		
+		if (filmWritten) {
+			
+			return ResponseEntity.status(HttpStatus.OK).build();
+			
+		}else {
+			
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
 	}
 }
--- src/main/java/pelis/model/Pelis.java
+++ src/main/java/pelis/model/Pelis.java
@@ -1,13 +1,20 @@
 package pelis.model;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.json.JSONObject;
-//import java.nio.file.Files;
 
 public class Pelis {
 	
-	static String pelisLocation = "./pelis";
+	private static File pelisLocation = new File("./pelis");
 
 	public String getAllPelis() {
 		
@@ -15,19 +22,175 @@ public class Pelis {
 		
 		if (isEmptyDirectory()) {
 			
-			result = new JSONObject().append("títulos", "").toString();
+			result = new JSONObject().append("titulos", "").toString();
 			
 		}else {
 			
-			result = null;
+			JSONObject pelisList = new JSONObject();
+			JSONObject peli = new JSONObject();
+			
+			List<String> fileLines = new ArrayList<String>();
+			
+			for (File file : pelisLocation.listFiles()) {
+				
+				if (file.canRead()) {
+					
+					fileLines.clear();
+					
+					try {
+						
+						fileLines = Files.readAllLines(file.toPath());
+						
+						if (fileLines.size() > 0) {
+							
+							peli.clear();
+							peli.put("id", removeExtension(file.getName()));
+							peli.put("titulo", fileLines.get(0).replace("Titulo: ", ""));
+							
+							pelisList.append("titulos", peli);
+						}
+						
+					} catch (IOException e) {
+						
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			result = pelisList.toString();
 		}
 		
 		return result;
 	}
 
+	public String getPeli(String filmID) {
+		
+		List<String> fileLines = new ArrayList<String>();
+		JSONObject peli = new JSONObject();
+		
+		for (File file : pelisLocation.listFiles()) {
+			
+			if (file.canRead()) {
+				
+				String fileName = removeExtension(file.getName());
+				
+				if (fileName.equals(filmID)) {
+					
+					try {
+						
+						fileLines = Files.readAllLines(file.toPath());
+						
+						if (fileLines.size() > 0) {
+							
+							peli.clear();
+							peli.put("id", fileName);
+							peli.put("titulo", fileLines.get(0).replace("Titulo: ", ""));
+							
+							fileLines.remove(0);
+							
+							peli.append("resenyas", fileLines);
+							
+							return peli.toString();
+						}
+						
+					} catch (IOException e) {
+						
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		
+		return null;
+	}
+
+	private static String removeExtension(String fileName) {
+		
+		String extensionArchivo = "";
+		
+		int extensionPosition = fileName.lastIndexOf(".");
+		
+		if (extensionPosition != -1) {
+		
+			extensionArchivo = fileName.substring(extensionPosition);
+		}
+		
+		return fileName.replace(extensionArchivo,"");
+	}
+
 	private boolean isEmptyDirectory() {
 		
-		return new File("./pelis").list().length == 0 ? true : false;
+		return pelisLocation.list().length == 0 ? true : false;
+	}
+
+	public boolean postFilmReview(String jsonContent) {
+		
+		JSONObject filmJson = new JSONObject(jsonContent);
+		
+		if (!filmJson.get("id").equals(null) && !filmJson.get("usuario").equals(null) && !filmJson.get("resenya").equals(null)) {
+			
+			if (!isEmptyDirectory()) {
+				
+				for (File file : pelisLocation.listFiles()) {
+					
+					if (file.canRead()) {
+						
+						String fileName = removeExtension(file.getName());
+						
+						if (fileName.equals(filmJson.get("id"))) {
+							
+							try {
+								
+								String review = "\n" + filmJson.get("usuario") + ": " + filmJson.get("resenya");
+								
+								Files.writeString(file.toPath(), review, StandardCharsets.UTF_8, StandardOpenOption.APPEND);
+								
+								return true;
+								
+							} catch (IOException e) {
+
+								e.printStackTrace();
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return false;
+	}
+
+	public boolean postFilm(String jsonContent) {
+		
+		JSONObject filmJson = new JSONObject(jsonContent);
+		
+		int filmID = 1;
+		
+		if (!filmJson.get("titulo").equals(null)) {
+			
+			if (!isEmptyDirectory()) {
+				
+				filmID = pelisLocation.listFiles().length + 1;
+			}
+			
+			String fileContent = "Título: " + filmJson.get("titulo");
+			String fileName = Integer.toString(filmID) + ".txt";
+			
+			Path fileLocation = new File(pelisLocation + File.separator + fileName).toPath();
+			
+			try {
+				
+				Files.writeString(fileLocation, fileContent, StandardCharsets.UTF_8);
+				
+				return true;
+				
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+		}
+		
+		return false;
 	}
 
 	
